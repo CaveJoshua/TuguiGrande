a0f1346dd9be3c1b4a80c1bb671ae2261ab4df41
CaveJoshua <nsec.fuhua.cv@gmail.com>
2025-10-04 13:30:32 +0800

    Delete test_app.py

--- test_app.py
+++
@@ -1,9 +0,0 @@
-def test_sample_pass():
-    """A simple test to make sure pytest is working."""
-    assert 1 + 1 == 2
-
-def test_barangay_data_route(client):
-    """Tests the main dashboard data endpoint."""
-    response = client.get('/barangay/data')
-    assert response.status_code == 200
-    assert 'stats' in response.json
